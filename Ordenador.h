4ee82fe64470cb64b532062626999683289ba029
gael <gael@GaelUbuntu.myguest.virtualbox.org>
2024-04-17 20:51:34 -0600

    Se agrega el metodo seleccion

--- Ordenador.h
+++ Ordenador.h
@@ -21,3 +21,16 @@ class Ordenador{
 	void radixsort(int *A, int n);
 };
 #endif
+
+
+void Ordenador::seleccion(int *A, int n){
+	for (int i = 1; i < n; i++) {
+        int key = A[i];
+        int j = i - 1;
+        while (j >= 0 && A[j] > key) {
+            A[j + 1] = A[j];
+            j = j - 1;
+        }
+        A[j + 1] = key;
+    }
+}

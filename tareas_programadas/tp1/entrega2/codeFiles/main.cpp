919ba9e392c8ad98ded72028efbc8893dc4ee332
gael <gael@GaelUbuntu.myguest.virtualbox.org>
2024-05-13 23:05:59 -0600

    Tests con las diferentes capacidades

--- tareas_programadas/tp1/entrega2/codeFiles/main.cpp
+++ tareas_programadas/tp1/entrega2/codeFiles/main.cpp
@@ -13,56 +13,75 @@ void generarArregloAleatorio(int *A, int n) {
 int main() {
      Ordenador ordenador;
 
-    // int tamanos[] = {50000, 100000, 150000, 200000};
+    int tamanos[] = {50000, 100000, 150000, 200000};
 
-    // // Número de ejecuciones por arreglo
-    // const int num_ejecuciones = 3;
+    // Número de ejecuciones por arreglo
+    const int num_ejecuciones = 3;
 
-    // // Abrir archivo de texto para escribir los resultados
-    // ofstream resultados("resultados.txt");
+    // Abrir archivo de texto para escribir los resultados
+    ofstream resultados("resultados.txt");
 
-    // for (int tam : tamanos) {
-    //     resultados << "Tamaño del arreglo: " << tam << endl;
+    for (int tam : tamanos) {
+        resultados << "Tamaño del arreglo: " << tam << endl;
         
-    //     int *arreglo = new int[tam];
+        int *arreglo = new int[tam];
 
-    //     // Ejecutar cada algoritmo varias veces
-    //     for (int i = 0; i < num_ejecuciones; ++i) {
+        // Ejecutar cada algoritmo varias veces
+        for (int i = 0; i < num_ejecuciones; ++i) {
 
-    //         // Generar arreglo aleatorio
-    //         generarArregloAleatorio(arreglo, tam);
+            // Generar arreglo aleatorio
+            generarArregloAleatorio(arreglo, tam);
 
-    //         // // Ejecutar y medir el tiempo de ejecución para cada algoritmo
-    //         // // Hay que hacerlo cada uno por separado (Se comentan todos y solo se descomenta
-    //         // // el de un algoritmo)
-    //         // auto start_seleccion = high_resolution_clock::now();
-    //         // ordenador.seleccion(arreglo, tam);
-    //         // auto stop_seleccion = high_resolution_clock::now();
-    //         // auto duration_seleccion = duration<double, std::milli>(stop_seleccion - start_seleccion);
+            // // Ejecutar y medir el tiempo de ejecución para cada algoritmo
+            // // Hay que hacerlo cada uno por separado (Se comentan todos y solo se descomenta
+            // // el de un algoritmo)
+            // auto start_seleccion = high_resolution_clock::now();
+            // ordenador.seleccion(arreglo, tam);
+            // auto stop_seleccion = high_resolution_clock::now();
+            // auto duration_seleccion = duration<double, std::milli>(stop_seleccion - start_seleccion);
 
-    //         // auto start_insercion = high_resolution_clock::now();
-    //         // ordenador.insercion(arreglo, tam);
-    //         // auto stop_insercion = high_resolution_clock::now();
-    //         // auto duration_insercion = duration<double, std::milli>(stop_insercion - start_insercion);
+            // auto start_insercion = high_resolution_clock::now();
+            // ordenador.insercion(arreglo, tam);
+            // auto stop_insercion = high_resolution_clock::now();
+            // auto duration_insercion = duration<double, std::milli>(stop_insercion - start_insercion);
 
 
-    //         auto start_mergesort = high_resolution_clock::now();
-    //         ordenador.mergesort(arreglo, tam);
-    //         auto stop_mergesort = high_resolution_clock::now();
-    //         auto duration_mergesort = duration<double, std::milli>(stop_mergesort - start_mergesort);
+            // auto start_mergesort = high_resolution_clock::now();
+            // ordenador.mergesort(arreglo, tam);
+            // auto stop_mergesort = high_resolution_clock::now();
+            // auto duration_mergesort = duration<double, std::milli>(stop_mergesort - start_mergesort);
 
-    //         // Imprimir y escribir los tiempos de ejecución en el archivo de resultados
-    //         //resultados << "Tiempo de ejecución (seleccion): " << duration_seleccion.count() << " ms" << endl;
-    //         //resultados << "Tiempo de ejecución (insercion): " << duration_insercion.count() << " ms" << endl;
-    //         resultados << "Tiempo de ejecución (mergesort): " << duration_mergesort.count() << " ms" << endl;
-    //     }
 
-    //     // Liberar memoria del arreglo
-    //     delete[] arreglo;
-    // }
+            // auto start_heapsort = high_resolution_clock::now();
+            // ordenador.heapsort(arreglo, tam);
+            // auto stop_heapsort = high_resolution_clock::now();
+            // auto duration_heapsort = duration<double, std::milli>(stop_heapsort - start_heapsort);
+            
 
-    // // Cerrar archivo de resultados
-    // resultados.close();
+            // auto start_quicksort = high_resolution_clock::now();
+            // ordenador.quicksort(arreglo, tam);
+            // auto stop_quicksort = high_resolution_clock::now();
+            // auto duration_quicksort = duration<double, std::milli>(stop_quicksort - start_quicksort);
+
+            auto start_radixsort = high_resolution_clock::now();
+            ordenador.radixsort(arreglo, tam);
+            auto stop_radixsort= high_resolution_clock::now();
+            auto duration_radixsort = duration<double, std::milli>(stop_radixsort - start_radixsort);
+            // Imprimir y escribir los tiempos de ejecución en el archivo de resultados
+            //resultados << "Tiempo de ejecución (seleccion): " << duration_seleccion.count() << " ms" << endl;
+            //resultados << "Tiempo de ejecución (insercion): " << duration_insercion.count() << " ms" << endl;
+            //resultados << "Tiempo de ejecución (mergesort): " << duration_mergesort.count() << " ms" << endl;
+            //resultados << "Tiempo de ejecución (heapsort): " << duration_heapsort.count() << " ms" << endl;
+            //resultados << "Tiempo de ejecución (quicksort): " << duration_quicksort.count() << " ms" << endl;
+            resultados << "Tiempo de ejecución (radixsort): " << duration_radixsort.count() << " ms" << endl;
+        }
+
+        // Liberar memoria del arreglo
+        delete[] arreglo;
+    }
+
+    // Cerrar archivo de resultados
+    resultados.close();
 
    
 
@@ -115,23 +134,5 @@ int main() {
     //     }
     // }
 
-    // Verificar si se ordena correctamente:
-    // Arreglo de prueba
-    int A[] = {12, 5, 8, 3, 9, 20, 15, 7};
-    int n = sizeof(A) / sizeof(A[0]);
-
-    // Prueba del algoritmo de selección
-    ordenador.heapsort(A, n);
-    cout << "Heapsort: ";
-    
-    for (int i = 1; i < n; i++){
-        if(A[i] < A[i-1]){
-            cout << "Error-" << ordenador.ImprimirDatosDeTarea();
-        } else {
-            cout << A[i] << ", ";
-        }
-    }
-    cout << endl;
-    
-    return 0;
+  return 0;
 }
